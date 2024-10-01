5bf52b74a2a79873cb2a75cf70bab8aa58cc4d64
KacperZSP3 <k.ksykiewicz@zsp3zamosc.pl>
2024-10-01 10:03:01 +0200

    Recipe Manager

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import RecipeTitle from "./RecipeTitle";
+import IngredientList from "./ IngredientList";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const recipe = {
+      title: "Mashed potatoes",
+      feedback: {
+          rating: 4.8,
+          reviews: 20
+      },
+      ingredients: [
+          { name: '3 potatoes, cut into 1/2" pieces', prepared: false },
+          { name: '4 Tbsp butter', prepared: false },
+          { name: '1/8 cup heavy cream', prepared: false },
+          { name: 'Salt', prepared: true },
+          { name: 'Pepper', prepared: true },
+      ],
+  }
+
+    return (
+      <article>
+        <h1>Recipe Manager</h1>
+          <RecipeTitle title = {recipe.title} feedback={recipe.feedback} />
+          <IngredientList ingredients={recipe.ingredients} />
+      </article>
+
   );
 }
 
